05db3919f2166c0a55e707d6a4240078f67c2164
aleary927 <learyaidan@gmail.com>
2024-10-15 15:55:52 -0600

    ALU outline

---
+++ verilog_files/ALU.v
@@ -0,0 +1,11 @@
+module ALU #(parameter DATA_WIDTH)
+  (
+  input [DATA_WIDTH - 1:0] a,
+  input [DATA_WIDTH - 1:0] b,
+  input [2:0] select,
+  output [DATA_WIDTH - 1:0] out,
+  output C, L, F, Z, N
+  );
+
+
+endmodule

f4d20513ec433b4bba53dd31f60656afef5efe8b
aleary927 <learyaidan@gmail.com>
2024-10-17 16:44:21 -0600

    minor error fixes in RF

--- verilog_files/RF.v
+++ verilog_files/RF.v
@@ -1,10 +1,10 @@
-module RF #(parameter DATA_WIDTH, REG_BITS = 3)
+module RF #(parameter DATA_WIDTH = 16, REGBITS = 3)
   (
     input clk,
     input wr_en,
-	  input [3:0] opcode,
+	  // input [3:0] opcode,
     input [DATA_WIDTH - 1:0] wr_data,
-    input [REG_BITS - 1:0] addr1, addr2,
+    input [REGBITS - 1:0] addr1, addr2,
 	  input C_in, L_in, F_in, Z_in, N_in,
 	  output reg C, L, F, Z, N, 
     output [DATA_WIDTH - 1:0] rd_data1, rd_data2
@@ -19,6 +19,14 @@ module RF #(parameter DATA_WIDTH, REG_BITS = 3)
 //	$readmemb("<path-to-your-file>/reg.dat", RAM); 
 //	$display("done with RF load"); 
 //	end
+
+  // load registers with all zeros
+  integer i;
+  initial begin 
+    for (i = 0; i < (1<<REGBITS); i = i + 1) begin
+      RAM[i] = 16'b0;
+    end
+  end
 
    // dual-ported register file
    //   read two ports combinationally

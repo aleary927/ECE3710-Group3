851f761ba0006955018a839380bd43ec6e655ecb
aleary927 <learyaidan@gmail.com>
2024-10-17 19:17:23 -0600

    added tests for updating flags

--- verilog_files/testbenches/tb_RF.v
+++ verilog_files/testbenches/tb_RF.v
@@ -6,12 +6,22 @@ module tb_RF();
 
   reg clk;
 
-  reg [2:0] addr1, addr2; // addresses
-  reg wr_en;           // write enable
+  reg [3:0] addr1, addr2; // addresses
+  reg wr_en, f_en;           // write enable
   reg [15:0] wr_data;    // write data
+  reg C, L, F, Z, N;      // flags
   wire [15:0] rd_data1, rd_data2; // read data
 
-  RF rf(.clk(clk), .wr_en(wr_en), .wr_data(wr_data), .addr1(addr1), .addr2(addr2), .rd_data1(rd_data1), .rd_data2(rd_data2));
+  RF rf(.clk(clk), .wr_en(wr_en), .f_en(f_en), .wr_data(wr_data), .addr1(addr1), .addr2(addr2), .rd_data1(rd_data1), .rd_data2(rd_data2), .C_in(C), .L_in(L), .F_in(F), .Z_in(Z), .N_in(N));
+  
+  // index of PSR register
+  localparam PSR = 15;
+  // index's of flag bits in PSR register
+  localparam C_IND = 0;
+  localparam L_IND = 2;
+  localparam F_IND = 5;
+  localparam Z_IND = 6;
+  localparam N_IND = 7;
 
   // generate clock signal
   initial begin
@@ -25,6 +35,12 @@ module tb_RF();
     addr2 = 0;
     wr_data = 0;
     wr_en = 0;
+    f_en = 0;
+    C = 0; 
+    L = 0; 
+    F = 0; 
+    Z = 0; 
+    N = 0; 
   end
 
   // main testbench code
@@ -40,19 +56,27 @@ module tb_RF();
     wr_en = 1;
     wr_data = 1000;
     #10;
-    if (rf.RAM[addr1] != wr_data)
+    if (rf.registers[addr1] != wr_data)
       $display("error: result not written to register when write enabled");
 
     // test not writing when write enable not high
     addr1 = 6;
     wr_en = 0;
     #10;
-    if (rf.RAM[addr1] == wr_data)
+    if (rf.registers[addr1] == wr_data)
       $display("error: unexpected write to register when write not enabled");
 
-
-    // perform write
+    // try writing to PSR register 
+    addr1 = PSR; 
+    wr_data = 2**16 - 1;
     wr_en = 1;
+    #10; 
+    if (rf.registers[addr1] == wr_data)
+      $display("error: writing to PSR register not blocked");
+
+    // perform write so that there is data for further tests
+    wr_en = 1;
+    wr_data = 45;
     #10;
     wr_en = 0;
 
@@ -64,24 +88,39 @@ module tb_RF();
     addr1 = 5;
     addr2 = 6;
     #10;
-    if (rd_data1 != rf.RAM[addr1] || rd_data2 != rf.RAM[addr2])
+    if (rd_data1 != rf.registers[addr1] || rd_data2 != rf.registers[addr2])
       $display("error: dual read did not function properly");
 
     // test reading from same register
     addr2 = addr1;
     #10;
-    if (rd_data1 != rf.RAM[addr1] || rd_data2 != rf.RAM[addr1])
+    if (rd_data1 != rf.registers[addr1] || rd_data2 != rf.registers[addr1])
       $display("error: reading from same address did not function properly");
 
     // =======================
     // TEST TAKING IN NEW FLAGS
-    // ======================= TODO
+    // ======================= 
 
     // test that flags are not changed when write to flags not enabled
+    C = 1;
+    L = 1;
+    F = 1;
+    Z = 1;
+    N = 1;
+    f_en = 0;
+    wr_en = 0;
+    #10;
+    if (rf.registers[PSR] != 0) 
+      $display("error: flags were written to when f_en low");
+    wr_en = 1;
+    if (rf.registers[PSR] != 0) 
+      $display("error: wr_en caused PSR to be written to (should only be written to on f_en)");
 
-    // test changing flags when alu op performed
-
-    // test changing flags when comparision op performed
+    // test that flags are updated when write to flags enabled
+    f_en = 1;
+    #10; 
+    if (rf.registers[PSR] == 0) 
+      $display("error: PSR not written to on f_en");
 
     $display("testbench complete");
   end

b0204d81713435d01c47c6dda74a8b75b0af235d
aleary927 <learyaidan@gmail.com>
2024-10-24 17:32:00 -0600

    added highest level file for entire system

---
+++ verilog_files/System.v
@@ -0,0 +1,18 @@
+/* 
+* Highest level file. 
+* This file links the CPU and all external components (I/O and memory).
+*/
+module System(
+  input clk,
+  input reset,
+
+  // vga outputs
+
+  // other I/O outputs / inputs
+); 
+
+  // CPU 
+  // I/O mapped memory 
+  // VGA controller
+
+endmodule

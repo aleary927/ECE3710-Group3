506da61f7ad1ee5bd63209ac7011e40219192314
aleary927 <learyaidan@gmail.com>
2024-10-24 17:20:23 -0600

    memory now dual port

--- verilog_files/Memory.v
+++ verilog_files/Memory.v
@@ -1,15 +1,15 @@
 /*
 * Module to represent memory for CR16 CPU.
 *
-* Reads continuously, writes on rising edge.
+* Dual port, syncronous read and write.
 */
 module Memory #(parameter DATA_WIDTH = 16, SIZE = 2**16)
 (
   input clk,
-  input wr_en,
-  input [$clog2(SIZE) - 1:0] addr, 
-  input [DATA_WIDTH - 1:0] wr_data, 
-  output [DATA_WIDTH - 1:0] rd_data
+  input wr_en1, wr_en2,
+  input [$clog2(SIZE) - 1:0] addr1, addr2, 
+  input [DATA_WIDTH - 1:0] wr_data1, wr_data2, 
+  output reg [DATA_WIDTH - 1:0] rd_data1, rd_data2
 );
 
   // memory
@@ -20,14 +20,25 @@ module Memory #(parameter DATA_WIDTH = 16, SIZE = 2**16)
     $readmemh("/path/to/file.dat", ram);
   end
 
-  // write on rising edge of clock 
+  // write and read on rising edge of clock
   always @(posedge clk) begin 
-    // only write on wr_en 
-    if (wr_en) 
-      ram[addr] <= wr_data;
-  end
+    // port 1
+    if (wr_en1) begin
+      ram[addr1] <= wr_data1;
+      rd_data1 <= wr_data1;
+    end
+    else begin
+      rd_data1 <= ram[addr1]; 
+    end
 
-  // continuous read
-  assign rd_data = ram[addr];
+    // port 2 
+    if (wr_en2) begin
+      ram[addr2] <= wr_data2;
+      rd_data2 <= wr_data2;
+    end
+    else begin 
+      rd_data2 <= ram[addr2];
+    end
+  end
 
 endmodule

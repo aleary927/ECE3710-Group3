24b59b61953010007f57f0d26ad9c12f59985a9a
aleary927 <learyaidan@gmail.com>
2024-10-28 22:10:30 -0600

    minor changes

--- verilog_files/PC_ALU.v
+++ verilog_files/PC_ALU.v
@@ -3,8 +3,9 @@
 */
 module PC_ALU(
   input [20:0] c_pc,      // current pc
-  input [15:0] imm,       // immediate value
-  input addr_mode,        // addressing mode
+  input [15:0] offset,       // sign extended immediate
+  input [15:0] target,   // absolute address
+  input [1:0] addr_mode,        // addressing mode
   output reg [20:0] n_pc  // next pc
   );
 
@@ -19,9 +20,9 @@ module PC_ALU(
       // add one for next instruction
       NEXT_INSTR: n_pc = c_pc + 1; 
       // add immediate for offset 
-      OFFSET: n_pc = c_pc + imm;
+      OFFSET: n_pc = c_pc + {{5{offset[15]}},offset};   // sign exted
       // use immediate for absolute
-      ABSOLUTE: n_pc = imm;
+      ABSOLUTE: n_pc = {5'b0,target};    // zero pad
       default: n_pc = c_pc;
     endcase
   end

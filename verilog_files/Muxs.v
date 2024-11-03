e04c37549a3e1e2de4d77b6f2aeeeed575a61d88
aleary927 <learyaidan@gmail.com>
2024-11-02 23:15:14 -0600

    added bigger muxes

--- verilog_files/Muxs.v
+++ verilog_files/Muxs.v
@@ -52,3 +52,44 @@ module Mux4 #(parameter DATA_WIDTH = 16)
   end
 
 endmodule
+
+// 5 inputs 
+module Mux5 #(parameter DATA_WIDTH = 16) 
+( 
+  input [DATA_WIDTH - 1:0] a, b, c, d, e, 
+  input [2:0] sel, 
+  output reg [DATA_WIDTH - 1:0] out
+);
+
+  always @(*) begin 
+    case (sel) 
+      3'h0: out = a; 
+      3'h1: out = b; 
+      3'h2: out = c; 
+      3'h3: out = d;
+      3'h4: out = e;
+      default: out = a;
+  end
+
+endmodule
+
+// 6 inputs 
+module Mux5 #(parameter DATA_WIDTH = 16) 
+( 
+  input [DATA_WIDTH - 1:0] a, b, c, d, e, f,
+  input [2:0] sel, 
+  output reg [DATA_WIDTH - 1:0] out
+);
+
+  always @(*) begin 
+    case (sel) 
+      3'h0: out = a; 
+      3'h1: out = b; 
+      3'h2: out = c; 
+      3'h3: out = d;
+      3'h4: out = e;
+      3'h5: out = f;
+      default: out = a;
+  end
+
+endmodule

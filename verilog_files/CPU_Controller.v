899314b0fbc1f5fe2b6fc55324f913c9481ca7c4
aleary927 <learyaidan@gmail.com>
2024-11-03 20:55:14 -0700

    added paramters

--- verilog_files/CPU_Controller.v
+++ verilog_files/CPU_Controller.v
@@ -28,7 +28,8 @@ module CPU_Controller(
   // Parameters 
   // **************************
 
-  // ALU function selects
+  // ----- ALU function selects -----
+
   localparam ADD = 4'b0000; 
   localparam SUB = 4'b0001; 
   localparam AND = 4'b0010; 
@@ -39,10 +40,59 @@ module CPU_Controller(
   localparam ASH = 4'b0111;
   localparam MUL = 4'b1000;
 
-  // Opcodes 
-  // TODO add opcodes and opcode_exts
+  // ----- Opcodes -----
+
+  localparam RS_RD_OP         = 4'b0000;    // most Rsrc / Rdest based instructions (these all go by opcode extensions)
+  localparam ANDI_OP          = 4'b0001;
+  localparam ORI_OP           = 4'b0010;
+  localparam XORI_OP          = 4'b0011;
+  localparam LD_ST_J_OP       = 4'b0100;    // LOAD, STOR, Jcond, JAL
+  localparam ADDI_OP          = 4'b0101;
+  // localparam ADDUI_OP         = 4'b0110;    // (not implemented)
+  // localparam ADDCI_OP         = 4'b0111;    // (not implemented)
+  localparam SH_OP            = 4'b1000;    // shift instructions (this all go by opcode extensions)
+  localparam SUBI_OP          = 4'b1001;
+  // localparam SUBCI_OP         = 4'b1010;    // not implemented
+  localparam CMPI_OP          = 4'b1011;
+  localparam BCOND_OP         = 4'b1100;
+  localparam MOVI_OP          = 4'b1101;
+  localparam MULI_OP          = 4'b1110;
+  localparam LUI_OP           = 4'b1111;
+
+  // ----- Opcode extensions -----
+
+  // Rsrc, Rdest extensions
+  localparam AND_EXT          = 4'b0001;
+  localparam OR_EXT           = 4'b0010;
+  localparam XOR_EXT          = 4'b0011;
+  localparam ADD_EXT          = 4'b0101;
+  localparam ADDU_EXT         = 4'b0110;
+  localparam ADDC_EXT         = 4'b0111;
+  localparam SUB_EXT          = 4'b1001;
+  localparam SUBC_EXT         = 4'b1010;
+  localparam CMP_EXT          = 4'b1011;
+  localparam MOV_EXT          = 4'b1101;
+  localparam MUL_EXT          = 4'b1110;
+  // shift extensions
+  localparam LSHI_EXT         = 4'b0000;    // LSB is don't care condition
+  localparam ASHUI_EXT        = 4'b0010;    // LSB is don't care condition
+  localparam LSH_EXT          = 4'b0100;
+  localparam ASHU_EXT         = 4'b0110;
+  // load, store, jump extensions
+  localparam LOAD_EXT         = 4'b0000; 
+  localparam LPR_EXT          = 4'b0001;
+  localparam SNXB_EXT         = 4'b0010;
+  localparam STOR_EXT         = 4'b0100;
+  localparam SPR_EXT          - 4'b0101;
+  localparam ZRXB_EXT         = 4'b0110;
+  localparam JAL_EXT          = 4'b1000;
+  localparam TBIT_EXT         = 4'b1010;
+  localparam JCOND_EXT        = 4'b1100;
+  localparam SCOND_EXT        = 4'b1101;
+  localparam TBITI_EXT        = 4'b1110;
   
-  // FSM states
+  // ----- FSM states -----
+ 
   localparam FETCH           = 4'b0001;  // Instruction Fetch
 	localparam DECODE          = 4'b0010;  // Decode
 	localparam ALULOAD         = 4'b0011;  // Load operands for ALU operation, could further separate this into more states to remove extra logic in assignment. 
@@ -86,63 +136,61 @@ module CPU_Controller(
 			  DECODE: 
 			
 				 case (opcode)
-					  4'b0000: begin
+					  RS_RD_OP: begin
 							case (opcode_ext)
-								 4'b0101:  next_state = ALULOAD; // ADD
-								 4'b0110:  next_state = ALULOAD; // ADDU
-								 4'b0111:  next_state = ALULOAD; // ADDC
-								 4'b1001:  next_state = ALULOAD; // SUB
-								 4'b1010:  next_state = ALULOAD; // SUBC
-								 4'b1011:  next_state = ALULOAD; // CMP
-								 4'b0001:  next_state = ALULOAD; // AND
-								 4'b0010:  next_state = ALULOAD; // OR
-								 4'b0011: next_state = ALULOAD; // XOR
-								
-								 4'b1101: next_state = DATAMOVE; // MOV
+								 ADD_EXT:  next_state = ALULOAD; // ADD
+								 // ADDU_EXT:  next_state = ALULOAD; // ADDU (not implemented)
+								 // ADDC_EXT:  next_state = ALULOAD; // ADDC (not implemented)
+								 SUB_EXT:  next_state = ALULOAD; // SUB
+								 SUBC_EXT:  next_state = ALULOAD; // SUBC
+								 CMP_EXT:  next_state = ALULOAD; // CMP
+								 AND_EXT:  next_state = ALULOAD; // AND
+								 OR_EXT:  next_state = ALULOAD; // OR
+								 ORI_EXT: next_state = ALULOAD; // XOR
+								 MOV_EXT: next_state = DATAMOVE; // MOV
 								 default: next_state = FETCH; 
 							endcase
 					  end
 			
-					  4'b0100: begin
+					  LD_ST_J_OP: begin
 							case (opcode_ext)
-								 4'b0100: next_state = ADDRCALC; // STOR
-								 4'b0000: next_state = ADDRCALC;  // LOAD
-								
-							    4'b0010: next_state = SIGNEXTEND ; // SNXB
-								 4'b0110: next_state = SIGNEXTEND ; // ZRXB
+								 STOR_EXT: next_state = ADDRCALC; // STOR
+								 LOAD_EXT: next_state = ADDRCALC;  // LOAD
+							   SNXB_EXT: next_state = SIGNEXTEND ; // SNXB
+								 ZRXB_EXT: next_state = SIGNEXTEND ; // ZRXB
 								 
-								 4'b1101: next_state = BRANCH; // Scond
-								 4'b1100: next_state = JUMP; // Jcond
-								 4'b1000: next_state = JUMP; // JAL
-								 4'b1010: next_state = REGWRITE; // TBIT
-								 4'b1110: next_state = REGWRITE; // TBITI
-								 4'b0001: next_state = REGWRITE; // LPR
-								 4'b0101: next_state = REGWRITE; // SPR
+								 SCOND_EXT: next_state = BRANCH; // Scond
+								 JCOND_EXT: next_state = JUMP; // Jcond
+								 JAL_EXT: next_state = JUMP; // JAL
+								 TBIT_EXT: next_state = REGWRITE; // TBIT
+								 TBITI_EXT: next_state = REGWRITE; // TBITI
+								 LPR_EXT: next_state = REGWRITE; // LPR
+								 SPR_EXT: next_state = REGWRITE; // SPR
 								 default: next_state = FETCH; // DI, EI, EXCP, RETX, unused cases
 							endcase
 					  end
 			
-					  4'b0101: next_state = ALULOAD; // ADDI
+					  ADDI_OP: next_state = ALULOAD; // ADDI
 			
-					  4'b0110: next_state = ALULOAD; // ADDUI
+					  // ADDUI_OP: next_state = ALULOAD; // ADDUI (not implemented)
 			
-					  4'b0111: next_state = ALULOAD; // ADDCI
+					  // ADDCI_OP: next_state = ALULOAD; // ADDCI (not implemented)
 			
-					  4'b1000: next_state = SHIFT; // LSH LSHI ASHU ASHUI
+					  SH_OP: next_state = SHIFT; // LSH LSHI ASHU ASHUI
 			
-					  4'b1001: next_state = ALULOAD; // SUBI
+					  SUBI_OP: next_state = ALULOAD; // SUBI
 			
-					  4'b1010: next_state = ALULOAD; // SUBCI
+					  // SUBCI_OP: next_state = ALULOAD; // SUBCI (not implemented)
 			
-					  4'b1011: next_state = ALULOAD; // CMPI
+					  CMPI_OP: next_state = ALULOAD; // CMPI
 			
-					  4'b1100: next_state = BRANCH; // Bcond
+					  BCOND_OP: next_state = BRANCH; // Bcond
 			
-					  4'b1101: next_state = DATAMOVE; // MOVI
+					  MOVI_OP: next_state = DATAMOVE; // MOVI
 			
-					  4'b1110: next_state = ALULOAD; // MULI
+					  MULI_OP: next_state = ALULOAD; // MULI
 			
-					  4'b1111: next_state = IMMLOAD; // LUI
+					  LUI_OP: next_state = IMMLOAD; // LUI
 			
 					  default: next_state = FETCH; // undefined opcodes
 				 endcase

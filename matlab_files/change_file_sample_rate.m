a492f428ebf09efd171f39a00e7e73147deeecc7
aleary927 <learyaidan@gmail.com>
2024-11-25 23:41:56 -0700

    added script to change sample rate of a file directly

---
+++ matlab_files/change_file_sample_rate.m
@@ -0,0 +1,22 @@
+function change_file_sample_rate(input_file, output_file, fs_out) 
+  % Take an input signal with given sample rate, and 
+  % print it out to file with new sample rate.
+
+  [original_signal, fs_in] = audioread(input_file);
+
+  if fs_in == fs_out 
+    disp("Sample rate matches orignal (%dHz).", fs_in)
+    disp("no action taken.")
+    return
+  end
+
+  resampled_signal = change_sample_rate(original_signal, fs_in, fs_out);
+
+  % resampled_signal = resampled_signal * (2^15 - 1) * max(resampled_signal);
+
+  fd = fopen(output_file, 'w'); 
+  binstr = gen_binstr(resampled_signal, 16);
+  fprintf(fd, "%s\n", binstr{:});
+  fclose(fd);
+
+end

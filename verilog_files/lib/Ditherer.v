8b06a0d5d35c631b3c39c3e1b507e761f8be3e8b
aleary927 <learyaidan@gmail.com>
2024-12-02 15:03:07 -0700

    fixed system verilog keyword conflict

--- verilog_files/lib/Ditherer.v
+++ verilog_files/lib/Ditherer.v
@@ -10,16 +10,16 @@ module Ditherer
   output [15:0] signal_out
 );
 
-  wire [7:0] rand;
+  wire [7:0] random;
 
   // add sign-extended 8-bit random number
-  assign signal_out = signal_in + {{8{rand[7]}}, rand};
+  assign signal_out = signal_in + {{8{random[7]}}, random};
 
   LFSR_8bit lfsr (
     .clk(clk), 
     .reset_n(reset_n), 
     .en(en), 
-    .rand(rand)
+    .random(random)
   );
 
 
--- verilog_files/lib/LFSR_8bit.v
+++ verilog_files/lib/LFSR_8bit.v
@@ -6,7 +6,7 @@ module LFSR_8bit
   input clk, 
   input reset_n, 
   input en,
-  output reg [7:0] rand
+  output reg [7:0] random
 );
 
   localparam START_BITS = 8'haa;
@@ -15,13 +15,13 @@ module LFSR_8bit
 
   always @(posedge clk) begin 
     if (!reset_n) 
-      rand <= START_BITS; 
+      random <= START_BITS; 
     else if (en)
-      rand <= {rand[6:0], feedback};
+      random <= {random[6:0], feedback};
   end
 
   // tap points found online: 
   // https://www.eetimes.com/tutorial-linear-feedback-shift-registers-lfsrs-part-1/
-  assign feedback = rand[1] ^ rand[2] ^ rand[3] ^ rand[7]; 
+  assign feedback = random[1] ^ random[2] ^ random[3] ^ random[7]; 
 
 endmodule
